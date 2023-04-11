ee7c9445d093065f4a81ebd887707044863cd0cc
JeyranHumbatova <7F49D9G@code.edu.az>
2023-04-11 19:38:59 +0400

    task1

---
+++ New folder/task1/assets/js/main.js
@@ -0,0 +1,53 @@
+/* var num=60;
+
+if (num>0 && num%3==0 && num%10==0) {
+    alert('3e ve 10a bolunur')
+}
+else {
+    alert('bolunmur')
+} */
+
+/* var num=44;
+
+if (num<0){
+    alert('not a grade')
+}
+else if (num<51) {
+    alert ('grade F')
+}
+else if (num<61) {
+    alert ('grade E')
+}
+else if (num<71) {
+    alert ('grade D')
+}
+else if (num<81) {
+    alert ('grade C')
+}
+else if (num<91) {
+    alert ('grade B')
+}
+else if (num<=100) {
+    alert ('grade A')
+}
+else {
+    alert ('not a grade')
+} */
+
+/* var x=22, y=6;
+
+if (x%2==0 && y%3==0 ) {
+    alert (x+y)
+}
+else {
+    alert('false')
+} */
+
+/* var num=prompt();
+
+if (isNaN && num<9 && num>100) { 
+    alert ('input true number')
+}
+else if () {
+    
+} */
